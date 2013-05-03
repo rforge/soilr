c766da417d4be0a0873b0c2e9492ac23b09304cf
markus <markus@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-05-03 16:24:44 +0000

    minimal changes to make the windows build run
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/soilr@54 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/inst/tests/runit.check.pass.R
+++ pkg/inst/tests/runit.check.pass.R
@@ -3,15 +3,31 @@ test.check.pass=function(){
 	prefix="../../R/"
 	globstring=paste(prefix,"*.R",sep="")
 	auto_paths=Sys.glob(globstring)
-	sapply(auto_paths,source)
+	for (p in auto_paths){
+		source(p,local=TRUE)
+	}
 	#create a vector of all the function names
 	X <- lsf.str()
 	#create a helper that checks for the pass argument in the functions 
 	#parameter list
 	has_pass=function(x){"pass" %in% names(formals(x))}
-	ind=sapply(X,has_pass)
+	ind=as.vector(sapply(X,has_pass))
+	print(ind)
 	#extract the functions that have such an argument
 	Xpass=X[ind]
-	#now these functions must be tested if the honor pass
+	#now we check if there is a test that checks that the "pass" argument
+	#To this end we define a caller that keeps book over the actually tested
+	#functions a
+	l=list()
+	passCaller=function(previousCallers,actualCall){
+		print("This is pass caller")
+		print(sys.call(which=1))
+		print(sys.function(which=1))
+	}
+	data(C14Atm_NH,HarvardForest14CO2)
+	years=seq(1901,2010,by=0.5)
+	cf=call("GaudinskiModel14",t=years,ks=c(kr=1/3,koi=1/1.5,koeal=1/4,koeah=1/80,kA1=1/3,kA2=1/75,kM=1/110),FcAtm=C14Atm_NH)
+	passCaller(l,cf)
+    
 
 }
