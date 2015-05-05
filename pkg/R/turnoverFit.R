95bf6ebe9069cc38f8208d9c0ba4fe55060e50b6
markus <markus@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-05-05 11:40:23 +0000

    merged back
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/soilr@96 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/turnoverFit.R
+++ pkg/R/turnoverFit.R
@@ -74,11 +74,9 @@ turnoverFit=structure(
      }
      ,
      ex=function(){
-          # To calculate the turnover time for a sample from a temperate forest soil
-          # remove uncomment the following lines!
-
-          #turnoverFit(obsC14=115.22, obsyr=2004.5, C0=2800, yr0=1900,
-          #                          In=473, Zone="NHZone2")
+          # Calculate the turnover time for a sample from a temperate forest soil
+          turnoverFit(obsC14=115.22, obsyr=2004.5, C0=2800, yr0=1900,
+                                    In=473, Zone="NHZone2")
           
      }
 )
